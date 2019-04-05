2ad5c0831beeda14a43f5019ce6610038b822e3a
nathanwhit <nw7ct@virginia.edu>
2019-04-05 12:44:42 -0400

    Add another convenience script

---
+++ deleteRemoteLogs.sh
@@ -0,0 +1,9 @@
+#!/usr/local/bin/zsh
+remote_log_dir='/var/mobile/Containers/Data/Application/CD8307A6-38FC-459E-9269-D13966195167/Documents/InspectiveC/Chrome'
+local_log_dir='./logs'
+ssh '-T' '-p' "${THEOS_DEVICE_PORT}" "root@${THEOS_DEVICE_IP}" << EOF
+    find "${remote_log_dir}" -name '*.log' -delete
+    exit
+EOF
+
+echo "All remote logs deleted"
