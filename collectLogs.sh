006ee2d0b645601827590a9dc0dd8d63283ef1d5
nathanwhit <nw7ct@virginia.edu>
2019-04-13 21:27:07 -0400

    Fix scripts

--- collectLogs.sh
+++ collectLogs.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/zsh
-remote_log_dir='/var/mobile/Containers/Data/Application/CD8307A6-38FC-459E-9269-D13966195167/Documents/InspectiveC/Chrome'
+remote_log_dir='/var/mobile/Containers/Data/Application/D4B762CC-0C3A-4073-B25A-E97A5A32E012/Documents/InspectiveC/Chrome'
 local_log_dir='./logs'
 scp -r -P "${THEOS_DEVICE_PORT}" "root@${THEOS_DEVICE_IP}:${remote_log_dir}/*.log" "${local_log_dir}/."
 echo "Logs collected"
