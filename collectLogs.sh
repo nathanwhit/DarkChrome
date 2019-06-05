2114040ca8b0dc86eb62a1d318552a84cee212be
nathanwhit <nwhit131@gmail.com>
2019-06-05 10:36:58 -0400

    Update scripts

--- collectLogs.sh
+++ collectLogs.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/zsh
-remote_log_dir='/var/mobile/Containers/Data/Application/DF1F3F16-A5E5-48DA-BB49-1696B37AAB0A/Documents/InspectiveC/Chrome'
+remote_log_dir='/var/mobile/Containers/Data/Application/EC02EA17-AA03-4B46-833E-09426639F946/Documents/InspectiveC/Chrome'
 local_log_dir='./logs'
 scp -r -P "${THEOS_DEVICE_PORT}" "root@${THEOS_DEVICE_IP}:${remote_log_dir}/*.log" "${local_log_dir}/."
 echo "Logs collected"
--- deleteRemoteLogs.sh
+++ deleteRemoteLogs.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/zsh
-remote_log_dir='/var/mobile/Containers/Data/Application/DF1F3F16-A5E5-48DA-BB49-1696B37AAB0A/Documents/InspectiveC/Chrome'
+remote_log_dir='/var/mobile/Containers/Data/Application/EC02EA17-AA03-4B46-833E-09426639F946/Documents/InspectiveC/Chrome'
 local_log_dir='./logs'
 ssh '-T' '-p' "${THEOS_DEVICE_PORT}" "root@${THEOS_DEVICE_IP}" << EOF
     find "${remote_log_dir}" -name '*.log' -delete
